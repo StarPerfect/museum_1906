f7ca49eb618bb31d9e6d83291414973dfdf7f83f
Corina Allen <StarPerfect@gmail.com>
2019-07-18 11:05:28 -0600

    Walk through with Ian and Meg

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
-  attr_reader :name, :exhibits, :patrons
+  attr_reader :name, :exhibits, :patrons, :revenue, :patrons_of_exhibits
 
   def initialize(name)
     @name = name
     @exhibits = []
     @patrons = []
+    @revenue = 0
+    @patrons_of_exhibits = {}
   end
 
   def add_exhibit(exhibit)
@@ -13,18 +15,43 @@ class Museum
 
   def recommend_exhibits(patron)
     @exhibits.find_all{ |exhibit| patron.interests.include?(exhibit.name) }.reverse
+    @exhibits.find_all{ |exhibit| patron.interests.include? exhibit.name }# ** THIS IS HOW IAN & MEG DID IT - BASED ON THE ORDER THE EXHIBITS LISTED THEM IN *NOT* THE ORDER THE OF PATRONS INTERESTS
   end
 
   def admit(patron)
     @patrons << patron
+    recommend_exhibits(patron)
+    .sort_by{|exhibit| exhibit.cost}
+    .reverse
+    .each do |exhibit|
+      if exhibit.cost <= patron.spending_money
+        patron.spending_money -= exhibit.cost
+        @revenue += exhibit.cost
+        @patrons_of_exhibits[exhibit] = [] unless @patrons_of_exhibits.key? exhibit
+        @patrons_of_exhibits[exhibit] << patron
+      end
+    end
   end
 
-  def patrons_by_exhibit_interest
-    exhibit_patrons = Hash.new(0)
-    @exhibits.map do |exhibit|
-        exhibit_patrons[exhibit] = @patrons
+  # def patrons_by_exhibit_interest
+  #   exhibit_patrons = Hash.new(0)
+  #   @exhibits.map do |exhibit|
+  #       exhibit_patrons[exhibit] = @patrons
+  #     end
+  #     # exhibit_patrons
+  #   end
+
+    def patrons_by_exhibit_interest
+      exhibit_interests = Hash.new
+      @exhibits.each do |exhibit|
+        exhibit_interests[exhibit] = []
       end
-      # exhibit_patrons
+      @patrons.each do |patron|
+        recommend_exhibits(patron).each do |exhibit|
+          exhibit_interests[exhibit] << patron
+        end
+      end
+      exhibit_interests
     end
   end
 #
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -10,11 +10,15 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
     @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
     @imax = Exhibit.new("IMAX", 15)
-    @bob = Patron.new("Bob", 20)
-    @bob.add_interest("Dead Sea Scrolls")
-    @bob.add_interest("Gems and Minerals")
+    @bob = Patron.new("Bob", 10)
+    # @bob.add_interest("Dead Sea Scrolls")
+    # @bob.add_interest(@dead_sea_scrolls.name)
+    # @bob.add_interest("Gems and Minerals")
+    # @bob.add_interest(@gems_and_minerals.name)
     @sally = Patron.new("Sally", 20)
-    @sally.add_interest("IMAX")
+  #  @sally.add_interest("IMAX")
+#    @sally.add_interest(@imax.name)
+    @morgan = Patron.new("Morgan", 15)
   end
 
   def test_museum_exists
@@ -24,6 +28,7 @@ class MuseumTest < Minitest::Test
   def test_attributes
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+    assert_equal 0, @dmns.revenue
   end
 
   def test_add_exhibit
@@ -38,8 +43,12 @@ class MuseumTest < Minitest::Test
     @dmns.add_exhibit(@gems_and_minerals)
     @dmns.add_exhibit(@dead_sea_scrolls)
     @dmns.add_exhibit(@imax)
+    @sally.add_interest("IMAX")
+    @bob.add_interest(@gems_and_minerals.name)
+    @bob.add_interest(@dead_sea_scrolls.name)
 
-    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@bob)
+  #  assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@bob)
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob) #***THIS IS HOW MEG & IAN DID
     assert_equal [@imax], @dmns.recommend_exhibits(@sally)
   end
 
@@ -53,8 +62,15 @@ class MuseumTest < Minitest::Test
   end
 
   def test_patrons_by_exhibit_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
     @dmns.admit(@bob)
     @dmns.admit(@sally)
+    @bob.add_interest(@dead_sea_scrolls.name)
+    @bob.add_interest(@gems_and_minerals.name)
+    @sally.add_interest(@dead_sea_scrolls.name)
 
     expected = {
       @gems_and_minerals => [@bob],
@@ -63,5 +79,43 @@ class MuseumTest < Minitest::Test
     }
 
     assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_admitting_user_reduces_spending_money
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @bob.add_interest(@dead_sea_scrolls.name)
+    @dmns.add_exhibit(@imax)
+    @sally.add_interest(@imax.name)
+    @sally.add_interest(@dead_sea_scrolls.name)
+    @dmns.admit(@bob)
+    @dmns.admit(@sally)
+
+    assert_equal 0, @bob.spending_money
+    assert_equal 5, @sally.spending_money
+  end
+
+  def test_patrons_of_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+
+    @morgan.add_interest(@gems_and_minerals.name)
+    @morgan.add_interest(@dead_sea_scrolls.name)
+    @bob.add_interest(@dead_sea_scrolls.name)
+    @bob.add_interest(@imax.name)
+    #@sally.add_interest(@gems_and_minerals.name)
+    @sally.add_interest(@imax.name)
+
+    @dmns.admit(@bob)
+    @dmns.admit(@morgan)
+    @dmns.admit(@sally)
+
+
+    expected = {
+      @dead_sea_scrolls => [@bob, @morgan],
+      @imax => [@sally],
+      @gems_and_minerals => [@morgan]
+    }
+    assert_equal expected, @dmns.patrons_of_exhibits
   end
 end
